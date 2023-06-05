a138039336aef5310e17b48cd5348c72e7a7a446
Andrea <andrearewards@hotmail.com>
2023-06-05 10:34:18 -0400

    implemenet authorization so only logged in users can use the app

--- main_app/views.py
+++ main_app/views.py
@@ -4,31 +4,35 @@ from .models import Show
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Define the home view
 class Home(LoginView):
   template_name = 'home.html'
 
+@login_required
 def show_index(request):
   shows = Show.objects.filter(user=request.user)
   return render(request, 'shows/index.html', {'shows': shows})
 
+@login_required
 def show_detail(request, show_id):
   show= Show.objects.get(id=show_id)
   return render(request, 'shows/detail.html', { 'show': show})
 
-class ShowCreate(CreateView):
+class ShowCreate(LoginRequiredMixin, CreateView):
   model = Show
   fields= ['name', 'genre', 'seasons', 'notes', 'rating', 'stillWatching']
   def form_valid(self, form):
     form.instance.user = self.request.user
     return super().form_valid(form)
 
-class ShowUpdate(UpdateView):
+class ShowUpdate(LoginRequiredMixin, UpdateView):
   model= Show
   fields= '__all__'
 
-class ShowDelete(DeleteView):
+class ShowDelete(LoginRequiredMixin, DeleteView):
   model= Show
   success_url= '/shows/'
 
--- showcase/settings.py
+++ showcase/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_URL = 'home'
 LOGIN_REDIRECT_URL = 'show-index'
 LOGOUT_REDIRECT_URL = 'home'
 
