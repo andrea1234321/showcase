7d02c7c416f22233667db7c04ec0897569c249df
Andrea <andrearewards@hotmail.com>
2023-06-05 10:30:49 -0400

    create a signup page, change index view to only show the users shows

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Show
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Define the home view
 class Home(LoginView):
   template_name = 'home.html'
 
 def show_index(request):
-  shows = Show.objects.all()
+  shows = Show.objects.filter(user=request.user)
   return render(request, 'shows/index.html', {'shows': shows})
 
 def show_detail(request, show_id):
@@ -17,7 +19,10 @@ def show_detail(request, show_id):
 
 class ShowCreate(CreateView):
   model = Show
-  fields= '__all__'
+  fields= ['name', 'genre', 'seasons', 'notes', 'rating', 'stillWatching']
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
 
 class ShowUpdate(UpdateView):
   model= Show
@@ -29,3 +34,17 @@ class ShowDelete(DeleteView):
 
 class Home(LoginView):
   template_name = 'home.html'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('show-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
