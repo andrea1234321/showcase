a584b423b4e2c7edb81099433ccde121c1fdd695
Andrea <andrearewards@hotmail.com>
2023-06-07 10:03:51 -0400

    remove commented out code

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Show(models.Model):
   name= models.CharField(max_length=100)
   genre= models.CharField(max_length=100)
@@ -12,7 +12,6 @@ class Show(models.Model):
   rating= models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
   stillWatching= models.BooleanField('Still watching?')
   url= models.URLField('URL link:', blank=True)
-  # url = models.CharField(max_length=250)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -6,13 +6,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-# import uuid
-# import boto3
 
-# S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
-# BUCKET = 'andrea-showcase'
-
-# Define the home view
 class Home(LoginView):
   template_name = 'home.html'
 
